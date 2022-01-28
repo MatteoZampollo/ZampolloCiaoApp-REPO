93ebebd73d113a31b5e11e2c7ca43a5878497478
MatteoZampollo <98151200+MatteoZampollo@users.noreply.github.com>
2022-01-28 09:52:40 +0100

    Aggiunto il saluto

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            Console.WriteLine("Dimmi il tuo nome: ");
+            nome= Console.ReadLine();
+            Console.WriteLine($"Ciao {nome}");
+        }
+    }
+}
